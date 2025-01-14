11fc173cda0d5c5954303fdb26274d0169f6d526
Parthib <parthibdhal11@.com>
2025-01-14 20:43:35 +0530

    added get_requirments() in setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,53 @@
 #F:/anaconda_installed/Scripts/activate
 #conda activate venv/
+from setuptools import setup, find_packages
+from typing import List
+
+
+
+def get_requirments()->List[str]:
+    requirment_lst:List[str]=[]
+    try:
+        with open('requirments.txt','r') as file:
+            lines=file.readlines()
+            for line in lines:
+                requirment=line.strip()
+                if requirment and requirment!='-e .':
+                    requirment_lst.append(requirment)
+
+    except FileNotFoundError:
+        print('requirments.txt not found')
+    return requirment_lst
+
+
+# print(get_requirments())
+
+setup(
+    name="NetworkSecurity",
+    version='0.0.1',
+    author_email='parthibdhal11@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirments()
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
